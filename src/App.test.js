f530c18120026e18fb9f6dbdfe03b32d390a14d4
Deepali Garg <deepali.garg@zennioptical.com>
2019-08-21 15:13:36 -0700

    writes a few beginning test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
+import { exportAllDeclaration } from "../node_modules/@babel/types";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @param {any} state - Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+
+const setUp = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+// test("renders without crashing", () => {
+//   const wrapper = shallow(<App />);
+//   console.log(wrapper.debug());
+// });
+
+test("renders increment button", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test("counter starts at 0", () => {});
+
+test("clicking button increments the counter display", () => {});
